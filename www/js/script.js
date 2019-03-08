9135b82ee43d9248d9954dd31beacb98c4943b74
piratezilla <tralockrox@gmail.com>
2019-03-08 20:47:54 +0000

    touch screen compatible

--- www/js/script.js
+++ www/js/script.js
@@ -3,8 +3,33 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 context.lineWidth = 5;
 var mouseDown = false;
+//From Here
+var mousePos = { x:0, y:0 };
+var lastPos = mousePos;
 
-canvas.addEventListener('mousemove', drawTool);
+canvas.addEventListener("touchstart", function (e) {
+  mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// to here
 
 canvas.addEventListener('mousedown', function()
 {
@@ -19,6 +44,8 @@ canvas.addEventListener('mouseup', function()
 { 
   mouseDown = false; 
 });
+
+canvas.addEventListener('mousemove', drawTool);
 
 canvas.addEventListener('mouseout', function() 
 { 
@@ -64,3 +91,20 @@ function rectangleTool(){
   context.rect(188, 50, 200, 100);
   context.stroke();
 	  }
+
+
+
+
+
+
+// and here
+
+    function getTouchPos(canvasDom, touchEvent) {
+      var rect = canvasDom.getBoundingClientRect();
+      return {
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top
+      };
+    }
+
+// to here
